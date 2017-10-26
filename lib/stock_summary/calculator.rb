843309dbb6d500df7ffbc3be29e68fb2ce171add
Misha Slyusarev <mik3weider@gmail.com>
2017-10-27 00:46:31 +0200

    Add Calculator

--- lib/stock_summary/calculator.rb
+++ lib/stock_summary/calculator.rb
@@ -1,4 +1,33 @@
 class StockSummary::Calculator
-  def calculate_return
+  ROUND_PRECISION = 2
+
+  def get_stock_summary(stock_prices)
+    @stock_prices = stock_prices
+
+    {
+      stock_return: stock_return,
+      maximum_drawdown: maximum_drawdown
+    }
   end
+
+  private
+
+    def stock_return
+      (@stock_prices.last - @stock_prices.first).round(ROUND_PRECISION)
+    end
+
+    def maximum_drawdown
+      max = prev_max = min = @stock_prices.first
+
+      @stock_prices.reduce(0) do |drawdown, price|
+        if price > max
+          prev_max = max
+          max = price
+        elsif price < min
+          min = price
+        end
+
+        drawdown = ((prev_max - min) / prev_max).round(ROUND_PRECISION)
+      end
+    end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,6 +1,22 @@
 require "spec_helper"
 
 RSpec.describe StockSummary::Calculator do
-  describe '#calculate_return' do
+  describe '#get_stock_summary' do
+    let(:stock_prices) do
+      [ 175.2, 172.3, 171.9, 176.7 ]
+    end
+
+    let(:correct_stock_summary) do
+      {
+        stock_return: 1.5,
+        maximum_drawdown: 0.02
+      }
+    end
+
+    subject(:summary) { described_class.new.get_stock_summary(stock_prices) }
+
+    it 'returns correct summary' do
+      expect(summary).to eq(correct_stock_summary)
+    end
   end
 end
