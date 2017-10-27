49173fc13217f201d25abd37f88959efddaf761b
Misha Slyusarev <mik3weider@gmail.com>
2017-10-27 12:27:38 +0200

    Add Slack class

--- lib/stock_summary/slack.rb
+++ lib/stock_summary/slack.rb
@@ -1,4 +1,18 @@
+require 'slack-ruby-client'
+
 class StockSummary::Slack
-  def push
+  SLACK_CHANNEL = '#general'
+
+  def initialize
+    Slack.configure do |config|
+      config.token = ENV['SLACK_TOKEN']
+      fail 'Missing SLACK_TOKEN environemnt variable' unless config.token
+    end
+
+    @client = Slack::Web::Client.new
+  end
+
+  def push(message)
+    @client.chat_postMessage(channel: SLACK_CHANNEL, text: message)
   end
 end
